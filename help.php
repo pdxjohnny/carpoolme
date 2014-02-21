b00a36ee9f07f504980ac0a7c11cae792560067d
pdxjohnny <johnandersenpdx@gmail.com>
2014-02-20 21:34:33 -0800

    2/20/14 - done for today

--- help.php
+++ help.php
@@ -1,23 +1,18 @@
-<?php
-
-echo '<strong>Start by setting your destination and leave time.</strong><br>
-Type your destination into the <input type="textbox" placeholder="Destination"> box and hit <button>Find</button><br>
-Click on the green checkered flag you want as your destination and hit <button>Set as destination</button><br>
+<strong>Start by setting your destination and leave time.</strong><br>
+Search your destination with the <input type="textbox" placeholder="Destination"> box. <br>
+Click on the green checkered flag<img src="images/mydest.png" style="height: 25px"/> you want as your destination and hit <button>Set as destination</button><br>
 Set your leave time with the <button>Update Leave Time</button> button.<br>
 
 <br><strong>Need a ride?</strong><br>
-If you&#39;re looking to find a ride click on the other checkered flags and hit <button>Ask for ride</button><br>
+If you're looking to find a ride click on the other checkered flags<img src="images/dest.png" style="height: 25px"/> and hit <button>Ask for ride</button><br>
 A request from you to enter that users car will be sent.<br>
-Once they accept you in their car you&#39;re good to go.<br>
+Once they accept you in their car you're good to go.<br>
 
 
 <br><strong>Offering a ride?</strong><br>
-After setting you&#39;re leave time and destination others will be able to ask you for a ride.<br>
-Ride offerers can still ask others for rides by clicking the other checkered flags and hiting the <button>Ask for ride</button> button.<br>
-Users who have asked you for a ride will be displayed with checkboxes to accept them into you&#39;re car or not.<br>
+After setting you're leave time and destination others will be able to ask you for a ride.<br>
+Ride offerers can still ask others for rides by clicking the other checkered flags<img src="images/dest.png" style="height: 25px"/> and hiting the <button>Ask for ride</button> button.<br>
+Users who have asked you for a ride will be displayed with checkboxes to accept them into you're car or not.<br>
 
 <br><strong>General</strong><br>
-If you no longer want to ride with someone click the <button>Remove me from my ride&#39;s car</button> button.<br>
-';
-
-?>
+If you no longer want to ride with someone click the <button>Remove me from my ride's car</button> button.<br>
