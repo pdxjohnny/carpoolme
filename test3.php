9d39d695dbe27fa4f7808de4621824328ba160ae
pdxjohnny <johnandersenpdx@gmail.com>
2014-02-27 21:35:01 -0800

    2/27/14 - Working on ajax upload

--- test3.php
+++ test3.php
@@ -1,12 +1,41 @@
-<script type="text/javascript">
+<form enctype="multipart/form-data">
+    <input name="file" type="file" />
+    <input type="button" value="Upload" />
+</form>
 
-function some_function(arg1, arg2, callback) {
-	var my_number = Math.ceil(Math.random() *
-		(arg1 - arg2) + arg2);
-	callback(my_number);
-}
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
 
-some_function(5, 15, function(num) {
-	console.log("callback called! " + num);
+$(':file').change(function(){
+    var file = this.files[0];
+    var name = file.name;
+    var size = file.size;
+    var type = file.type;
+    //Your validation
 });
+
+
+$(':button').click(function(){
+    var formData = new FormData($('form')[0]);
+    $.ajax({
+        url: 'test4.php',  //Server script to process data
+        type: 'POST',
+        xhr: function() {  // Custom XMLHttpRequest
+            var myXhr = $.ajaxSettings.xhr();
+            if(myXhr.upload){ // Check if upload property exists
+            }
+            return myXhr;
+        },
+        //Ajax events
+	data: formData,
+	success: function(data){
+		console.log(data);
+		},
+        //Options to tell jQuery not to process data or worry about content-type.
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+});
+
 </script>
--- test4.php
+++ test4.php
@@ -1,20 +1,31 @@
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script>
-var jsSnearby = [];
-nearby(function(){
-	console.log(jsSnearby);
-	});
-function nearby(callback){
-	document.write("starting<br>");
-	$.ajax({
-		type: "GET",
-		url: "test"+"/nearby.php",
-		data: {},
-		success: function(data){
-			jsSnearby = JSON.parse(data);
-			callback();
-			}
-		});
-	}
-</script>
+<?php
+$upload_dir = "/var/www/carpool/profiles/pictures/";
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+$maxsize = 2000000;
+if ($_FILES["file"]["size"] < $maxsize)
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    if(($_FILES["file"]["error"])==4) echo "No file selected";
+    else echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    if (file_exists($upload_dir . $_FILES["file"]["name"]))
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"], $upload_dir . $_FILES["file"]["name"]);
+      echo "File replaced";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"], $upload_dir . $_FILES["file"]["name"]);
+      echo "File uploaded";
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+?>
