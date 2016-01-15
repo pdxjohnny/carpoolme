ebacc67d05ce15fccebffbfca91548e2e3b706cf
John Andersen <johnandersenpdx@gmail.com>
2016-01-14 16:32:58 -0800

    Made all http into https
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- ChangeLog.php
+++ ChangeLog.php
@@ -31,7 +31,7 @@ Author: John Andersen
 	<link rel="stylesheet" href="stylesheets/layout.css">
 
 	<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 
 	<!-- Favicons
--- home.php
+++ home.php
@@ -32,7 +32,7 @@ Author: John Andersen
 	<link rel="stylesheet" href="stylesheets/layout.css">
 
 	<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 
 	<!-- Favicons
